7e674059686d511739a09716441ebb106198e1f1
HuShaoQiong <1043914966@qq.com>
2026-03-14 18:10:21 +0800

    nextauth

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -8,21 +8,47 @@ const sql = postgres(process.env.POSTGRES_URL!, { ssl: 'require' });
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),    //强制转换（改变）从字符串到数字
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        message: 'Please select a customer.',
+    }),
+    amount: z.coerce
+        .number()
+        .gt(0, { message: 'Please enter an amount greater than $0.' }),
+    status: z.enum(['pending', 'paid'], {
+        message: 'Please select an invoice status.',
+    }),
     date: z.string(),
 });
 
 // 新规则省略 id 和 data 属性
 const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+}
+
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     /**
      *  new Data() 创建一个当前日期对象 Sat Mar 14 2026 14:15:00 GMT+0800
@@ -32,18 +58,10 @@ export async function createInvoice(formData: FormData) {
      *  .split('T')[0] 以T为分隔符，将字符串转为数组并取第一个
      */
     const date = new Date().toISOString().split('T')[0];
-
-    try {
-        await sql`
+    await sql`
         INSERT INTO invoices (customer_id, amount, status, date)
         VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
       `
-    } catch (error) {
-        console.log(error)
-
-        return { message: 'Database Error: Failed to Create Invoice.' }
-    }
-
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices')
@@ -59,25 +77,17 @@ export async function updateInvoice(id: string, formData: FormData) {
     })
 
     const amountInCents = amount * 100;
-
-    try {
-        await sql`
+    await sql`
         UPDATE invoices
         SET customer_id = ${customerId}, amount = ${amountInCents}, status = ${status}
         WHERE id = ${id}
       `;
-    } catch (error) {
-        // We'll also log the error to the console for now
-        console.error(error);
-        // return { message: 'Database Error: Failed to Update Invoice.' };
-    }
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
 export async function deleteInvoice(id: string) {
-    // throw new Error('Failed to Delete Invoice');
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
 }
